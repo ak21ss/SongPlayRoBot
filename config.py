8758b5324867029bf6a1c4218452f54fb467d1c9
ak21ss <102480912+ak21ss@users.noreply.github.com>
2022-03-28 00:12:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-class Config:
-  API_ID = "11341733"
- API_HASH = "a8d0e82a1d69196c018caeacc6ee389f"
- BOT_TOKEN = "5234085443:AAH3QO_7ynvsZNDinpiyE-UVOpfuISmXJyA"
+import os
+API_ID = int(os.getenv("11341733"))
+API_HASH = os.getenv("a8d0e82a1d69196c018caeacc6ee389f") 
+BOT_TOKEN = os.getenv("5234085443:AAH3QO_7ynvsZNDinpiyE-UVOpfuISmXJyA")
+DATABASE_URL = os.getenv("DATABASE_URL")
+OWNER_ID = list({int(x) for x in os.environ.get("5273435668", "").split()})
 
