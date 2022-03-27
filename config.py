8da2a2edbddaedae2b3232d4eae29114481aff13
ak21ss <102480912+ak21ss@users.noreply.github.com>
2022-03-28 00:03:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import os
-API_ID = int(os.getenv("11341733"))
-API_HASH = os.getenv("a8d0e82a1d69196c018caeacc6ee389f") 
-BOT_TOKEN = os.getenv("5234085443:AAH3QO_7ynvsZNDinpiyE-UVOpfuISmXJyA")
+class Config:
+  API_ID = "11341733"
+ API_HASH = "a8d0e82a1d69196c018caeacc6ee389f"
+ BOT_TOKEN = "5234085443:AAH3QO_7ynvsZNDinpiyE-UVOpfuISmXJyA"
 
