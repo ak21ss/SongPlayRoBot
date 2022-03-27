3427c8b70e08595e2bb4f1ae12b7fd9b07527aa4
ak21ss <102480912+ak21ss@users.noreply.github.com>
2022-03-27 23:53:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ import os
 API_ID = int(os.getenv("11341733"))
 API_HASH = os.getenv("a8d0e82a1d69196c018caeacc6ee389f") 
 BOT_TOKEN = os.getenv("5234085443:AAH3QO_7ynvsZNDinpiyE-UVOpfuISmXJyA")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+
